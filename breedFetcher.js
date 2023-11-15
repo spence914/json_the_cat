dc51129ed51c9162b96a6c6c85b176cb70b80427
Spencer Lewis <spence.914@gmail.com>
2023-11-14 16:58:31 -0800

    change breed inputted to be a command line argument, log only the description of the searched breed, not the entire object returned by the request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,16 @@
 const request = require('request');
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=sib"
+const breedToSearch = process.argv[2];
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedToSearch}`;
 
 request(URL, (error, response, body) => {
+  if (error) {
+    console.log('Error Occurred:', error);
+  }
+
+
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  if (data.length === 0) {
+    console.log("Invalid Breed, try again");
+  } else
+    console.log(data[0].description);
 });
